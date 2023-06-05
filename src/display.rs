af9c5f03a4113efa80df94e5c2c3d38e04c635fb
Debasish Ray Chawdhuri <debasish.raychawdhuri@gmail.com>
2023-06-06 00:24:49 +0530

    better clock faces

--- src/display.rs
+++ src/display.rs
@@ -5,6 +5,23 @@ pub trait Displayable {
     fn display(&self);
 }
 
+fn clock_face(event: &Event) -> char {
+    let hour = event.date_time.hour;
+    let minute = event.date_time.minute;
+    let rounded_hour = if minute > 45 { hour + 1 } else { hour };
+    let rounded_hour = if rounded_hour > 12 {
+        rounded_hour - 12
+    } else {
+        rounded_hour
+    };
+    let rounded_minute = if minute > 45 || minute < 15 { 0 } else { 30 };
+    if rounded_minute == 0 {
+        return char::from_u32('\u{1F54F}' as u32 + rounded_hour as u32).unwrap();
+    } else {
+        return char::from_u32('\u{1F55B}' as u32 + rounded_hour as u32).unwrap();
+    }
+}
+
 impl Displayable for Event {
     fn display(&self) {
         println!(
@@ -12,7 +29,7 @@ impl Displayable for Event {
             format!("\u{1F4C5}{}", self.date_time.year).yellow(),
             format!("{:02}", self.date_time.month).yellow(),
             format!("{:02}", self.date_time.day).yellow(),
-            format!("\u{1f551}{:02}", self.date_time.hour).green(),
+            format!("{}{:02}", clock_face(self), self.date_time.hour).green(),
             format!("{:02}", self.date_time.minute).green(),
         );
         let sanitized = self.sanitize();
